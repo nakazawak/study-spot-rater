bb5d96138cfdfa32f8538e69983f112e5d9478da
Koji Nakazawa <kojinakazawa129@gmail.com>
2025-04-30 15:18:21 -0700

    Project created

---
+++ src/main/java/dev/kojinaka/study_spot_rater/StudySpotRaterApplication.java
@@ -0,0 +1,13 @@
+package dev.kojinaka.study_spot_rater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpotRaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpotRaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kojinaka/study_spot_rater/StudySpotRaterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kojinaka.study_spot_rater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpotRaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
